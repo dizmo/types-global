f9f698e14c46afcfd484a61ff2cc23e1f3aa2c6c
Hasan Karahan <hasan.karahan@blackhan.com>
2018-11-01 16:41:53 +0300

    fixed package description

--- test/test.ts
+++ test/test.ts
@@ -18,12 +18,3 @@ describe("Global", () => {
         expect(global.key).to.be.an("undefined");
     });
 });
-
-describe("Global", () => {
-    it("should declare showBack", () => {
-        expect(global.showBack).to.be.an("undefined");
-    });
-    it("should declare showFront", () => {
-        expect(global.showFront).to.be.an("undefined");
-    });
-});
