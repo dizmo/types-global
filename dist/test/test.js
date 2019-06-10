2ba312239cd73d87bfef46a2a04d9c339d48896c
Hasan Karahan <hasan.karahan@blackhan.com>
2019-06-10 18:03:10 +0300

    yo @dizmo/module --upgrade

--- dist/test/test.js
+++ dist/test/test.js
@@ -1,24 +1,29 @@
 "use strict";
 
-var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
+function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
 
-Object.defineProperty(exports, "__esModule", { value: true });
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
 var chai_1 = require("chai");
+
 require("mocha");
+
 describe("Global", function () {
-    it("should exist", function () {
-        chai_1.expect(typeof global === "undefined" ? "undefined" : _typeof(global)).to.not.eq("undefined");
-    });
-    it("should be an object", function () {
-        chai_1.expect(typeof global === "undefined" ? "undefined" : _typeof(global)).to.eq("object");
-    });
-    it("should be referenceable", function () {
-        chai_1.expect(global).to.not.eq(undefined);
-    });
+  it("should exist", function () {
+    chai_1.expect(typeof global === "undefined" ? "undefined" : _typeof(global)).to.not.eq("undefined");
+  });
+  it("should be an object", function () {
+    chai_1.expect(typeof global === "undefined" ? "undefined" : _typeof(global)).to.eq("object");
+  });
+  it("should be referenceable", function () {
+    chai_1.expect(global).to.not.eq(undefined);
+  });
 });
 describe("Global", function () {
-    it("should allow arbitary entries", function () {
-        chai_1.expect(global.key).to.be.an("undefined");
-    });
+  it("should allow arbitary entries", function () {
+    chai_1.expect(global.key).to.be.an("undefined");
+  });
 });
 //# sourceMappingURL=test.js.map
